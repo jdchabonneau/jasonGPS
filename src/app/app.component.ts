d1e48f0b9415442473f309e12717f6d125579bef
jdc3 <Jack@Apples-MacBook-Air-2.local>
2017-09-29 19:54:09 +0700

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+  onCornerClick(cornerPosition) {
+    //console.log(cornerPosition);
+    this.getLocation();
+  }
+   getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+    }
+    showPosition(position) {
+      //x.innerHTML = "Latitude: " + position.coords.latitude + 
+      //"<br>Longitude: " + position.coords.longitude; 
+      console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+    }
+  }
+
+
